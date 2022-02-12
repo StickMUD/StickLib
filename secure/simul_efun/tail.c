6e4f0a581219a9cea7a8f2209063cc5ab6356df8
Mike Conley <sousesider@gmail.com>
2022-02-12 11:10:45 -0500

    Updated simul_efuns

--- secure/simul_efun/tail.c
+++ secure/simul_efun/tail.c
@@ -7,28 +7,57 @@
 
 #define TAIL_MAX_BYTES 1000
 
-varargs int tail(string file)
+varargs int
+tail(string file)
 {
-    if (extern_call())
-	set_this_object(previous_object());
+    //if (extern_call())
+	//set_this_object(previous_object());
+#if __VERSION__ >= "3.5.99"
+    if (!stringp(file) || !efun::this_player()) {
+	return 0;
+    }
 
-    if (!stringp(file) || !this_player())
+    bytes txt = read_bytes(file, -(TAIL_MAX_BYTES + 80), (TAIL_MAX_BYTES + 80));
+
+    if (!bytesp(txt)) {
 	return 0;
+    }
+
+    // cut off first (incomplete) line
+    int index = strstr(txt, b"\n");
+
+    if (index > -1) {
+	if (index + 1 < sizeof(txt)) {
+	    txt = txt[index+1..];
+	} else {
+	    txt = b"";
+	}
+    }
+
+    tell_object(efun::this_player(), to_text(txt, "UTF-8"));
+#else
+    if (!stringp(file) || !this_player()) {
+	return 0;
+    }
+
     string txt = read_bytes(file, -(TAIL_MAX_BYTES + 80), (TAIL_MAX_BYTES + 80));
-    if (!stringp(txt))
+
+    if (!stringp(txt)) {
 	return 0;
+    }
 
     // cut off first (incomplete) line
     int index = strstr(txt, "\n");
+
     if (index > -1) {
-	if (index + 1 < sizeof(txt))
+	if (index + 1 < sizeof(txt)) {
 	    txt = txt[index+1..];
-	else
+	} else {
 	    txt = "";
+	}
     }
 
-    tell_object(this_player(), txt);
-
+    tell_object(efun::this_player(), txt);
+#endif
     return 1;
 }
-
